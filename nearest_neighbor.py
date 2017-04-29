4d0ac63f7a9fac12fc3a865d2c206190a4afbd31
Kaitlyn Shih <kaitlynshih@Kaitlyns-MacBook-Pro.local>
2017-04-28 23:00:29 -0700

    added output file

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -4,10 +4,11 @@ import re
 import time
 
 pointRE=re.compile("(-?\\d+.?\\d*)\\s(-?\\d+.?\\d*)")
+
 def merge(left, right, v):
-	result = []
+	result = []		#store sorted points
 	i, j = 0, 0
-	while (len(result) < len(left) + len(right)):
+	while (len(result) < len(left) + len(right)):	#merge start
 		if left[i][v] < right[j][v]:
 			result.append(left[i])
 			i+= 1
@@ -24,8 +25,8 @@ def mergeSort(points, v):
         return points
 
     m = int(len(points)/2)
-    left = mergeSort(points[:m], 0)
-    right = mergeSort(points[m:], 0)
+    left = mergeSort(points[:m], v)		#sort left half
+    right = mergeSort(points[m:], v)	#sort right half
     return merge(left, right, v)
 
 def dist(p1, p2):
@@ -42,24 +43,25 @@ def nearest_neighbor_recursion(points):
         left = points[:m]       #left half of points
         right = points[m:]      #right half of points
 
-        med = right[0]
+        med = right[0]			#middle point
 
-        ld  = nearest_neighbor_recursion(left)
-        rd  = nearest_neighbor_recursion(right)
+        ld  = nearest_neighbor_recursion(left)		#min dist for left
+        rd  = nearest_neighbor_recursion(right)		#min dist for right
         if ld <= rd:
             min_distance = ld
         else:
             min_distance = rd
 
-        mid = []
-        ysort = mergeSort(points, 1)
+        mid = []						#list of points for the strip in middle
+        ysort = mergeSort(points, 1)	#sorted points by y
 
-        for p in ysort:
+        for p in ysort:					#populate mid with points
                 if abs(p[0] - med[0]) < min_distance:
                         mid.append(p)
 
         size = len(mid)
-        if size > 1:
+
+        if size > 1:					#search for smaller distance
                 for i in range(size - 1):
                         for j in range(i+1, min(i+8, size)):
                                 if dist(mid[i],mid[j]) < min_distance:
@@ -68,19 +70,22 @@ def nearest_neighbor_recursion(points):
 
 #Run the divide-and-conquor nearest neighbor
 def nearest_neighbor(points):
-    points = mergeSort(points, 0)
-    return nearest_neighbor_recursion(points)
+    start_time = time.clock()
+    points = mergeSort(points, 0)		#sort points by x
+    min_distance = nearest_neighbor_recursion(points)
+    print("--- %s seconds ---" % (time.clock() - start_time))
+    return min_distance
 
 #Brute force version of the nearest neighbor algorithm, O(n**2)
 def brute_force_nearest_neighbor(points):
-    start_time = time.clock()
+    # start_time = time.clock()
     min_distance=-1
     for i in range (0, len(points) - 1):
         for j in range (i+1, len(points)):
             d = dist(points[i], points[j])
             if (min_distance == -1) or (min_distance > d):
                 min_distance = d
-    print("--- %s seconds ---" % (time.clock() - start_time))
+    # print("--- %s seconds ---" % (time.clock() - start_time))
     return min_distance
 
 def read_file(filename):
@@ -102,6 +107,11 @@ def read_file(filename):
 def main(filename,algorithm):
     algorithm=algorithm[0:]
     points=read_file(filename)
+
+    newName = filename.replace(".txt", "_distance.txt")
+    file = open(newName, "w")
+    dis = str(nearest_neighbor(points))
+    file.write(dis)
     if algorithm =='dc':
         print("Divide and Conquer: ", nearest_neighbor(points))
     if algorithm == 'bf':
