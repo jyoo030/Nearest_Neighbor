6e013e1312623348a2d0755d430d002d4a666915
jyoo030 <jyoo030@ucr.edu>
2017-04-28 20:43:32 -0700

    Added text files and added bruteforce algorithm to nearest_neighbor

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -7,17 +7,15 @@ pointRE=re.compile("(-?\\d+.?\\d*)\\s(-?\\d+.?\\d*)")
 def dist(p1, p2):
     return sqrt(pow(p1[0]-p2[0],2) + pow(p1[1]-p2[1],2))
 
-#Run the divide-and-conquor nearest neighbor
-def nearest_neighbor(points):
-    return nearest_neighbor_recursion(points)
-
 #Brute force version of the nearest neighbor algorithm, O(n**2)
 def brute_force_nearest_neighbor(points):
-    min_distance=0
-    return min_distance
-
-def nearest_neighbor_recursion(points):
-    min_distance=0
+    min_distance=-1
+    for i in range (0, len(points) - 1):
+        for j in range (i+1, len(points)):
+            d = dist(points[i], points[j])
+            if (min_distance == -1) or (min_distance > d):
+                min_distance = d
+    print(min_distance)
     return min_distance
 
 def read_file(filename):
@@ -31,14 +29,14 @@ def read_file(filename):
         line = line.strip()
         point_match=pointRE.match(line)
         if point_match:
-            x = point_match.group(1)
-            y = point_match.group(2)
+            x = float(point_match.group(1))
+            y = float(point_match.group(2))
             points.append((x,y))
     print(points)
     return points
 
 def main(filename,algorithm):
-    algorithm=algorithm[1:]
+    algorithm=algorithm[0:]
     points=read_file(filename)
     if algorithm =='dc':
         print("Divide and Conquer: ", nearest_neighbor(points))
@@ -48,11 +46,12 @@ def main(filename,algorithm):
         print("Divide and Conquer: ", nearest_neighbor(points))
         print("Brute Force: ", brute_force_nearest_neighbor(points))
 
+
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print("python assignment1.py -<dc|bf|both> <input_file>")
+        print("python nearest_neighbor.py -<dc|bf|both> <input_file>")
         quit(1)
     if len(sys.argv[1]) < 2:
-        print("python assignment1.py -<dc|bf|both> <input_file>")
+        print("python nearest_neighbor.py -<dc|bf|both> <input_file>")
         quit(1)
     main(sys.argv[2],sys.argv[1])
