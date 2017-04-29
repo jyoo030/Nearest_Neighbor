6c665a91ba8517411230317c35f8cdfa260c262d
Kaitlyn Shih <kaitlynshih@Kaitlyns-MacBook-Pro.local>
2017-04-28 22:00:59 -0700

    updated nearest neighbor recursion

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -3,11 +3,11 @@ from math import sqrt
 import re
 
 pointRE=re.compile("(-?\\d+.?\\d*)\\s(-?\\d+.?\\d*)")
-def merge(left, right):
+def merge(left, right, v):
 	result = []
 	i, j = 0, 0
 	while (len(result) < len(left) + len(right)):
-		if left[i][0] < right[j][0]:
+		if left[i][v] < right[j][v]:
 			result.append(left[i])
 			i+= 1
 		else:
@@ -18,25 +18,56 @@ def merge(left, right):
 			break
 	return result
 
-def mergeSort(points):
+def mergeSort(points, v):
     if len(points) < 2:
         return points
 
     m = int(len(points)/2)
-    left = mergeSort(points[:m])
-    right = mergeSort(points[m:])
-    return merge(left, right)
+    left = mergeSort(points[:m], 0)
+    right = mergeSort(points[m:], 0)
+    return merge(left, right, v)
 
 def dist(p1, p2):
     return sqrt(pow(p1[0]-p2[0],2) + pow(p1[1]-p2[1],2))
 
 def nearest_neighbor_recursion(points):
-     min_distance=0
-     return min_distance
+    min_distance=0
+    num = len(points)
+
+    if num <= 3:
+        return brute_force_nearest_neighbor(points)
+    else:
+        m = int(num/2)          #middle index
+        left = points[:m]       #left half of points
+        right = points[m:]      #right half of points
+
+        med = right[0]
+
+        ld  = nearest_neighbor_recursion(left)
+        rd  = nearest_neighbor_recursion(right)
+        if ld <= rd:
+            min_distance = ld
+        else:
+            min_distance = rd
+
+        mid = []
+        ysort = mergeSort(points, 1)
+
+        for p in ysort:
+                if abs(p[0] - med[0]) < min_distance:
+                        mid.append(p)
+
+        size = len(mid)
+        if size > 1:
+                for i in range(size - 1):
+                        for j in range(i+1, min(i+8, size)):
+                                if dist(mid[i],mid[j]) < min_distance:
+                                        min_distance = dist(mid[i], mid[j])
+    return min_distance
 
 #Run the divide-and-conquor nearest neighbor
 def nearest_neighbor(points):
-    points = mergeSort(points) 
+    points = mergeSort(points, 0)
     return nearest_neighbor_recursion(points)
 
 #Brute force version of the nearest neighbor algorithm, O(n**2)
@@ -47,7 +78,6 @@ def brute_force_nearest_neighbor(points):
             d = dist(points[i], points[j])
             if (min_distance == -1) or (min_distance > d):
                 min_distance = d
-    print(min_distance)
     return min_distance
 
 def read_file(filename):
