ece2f03a6002b9d49978cbd026b2926a7cd0fdd4
jyoo030 <jyoo030@ucr.edu>
2017-04-28 23:37:48 -0700

    testing

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Nearest_Neighbor
-Assignment 1 for CS141, finding the distance between the closest pair of points for a large set of data.
+To run the program, open a terminal and enter python, then enter the name of the .py file (nearest_neighbor.py) and put the filename right after.
+For example, if the file containing your data is "input.txt", then you would put python nearest_neighbor.py input.txt. 
+This will output a file that is titled "input_distance.txt" with the answer. It will always be the original filename_distance.
--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -4,43 +4,21 @@ import re
 import time
 
 pointRE=re.compile("(-?\\d+.?\\d*)\\s(-?\\d+.?\\d*)")
-def merge(left, right, v):
-	result = []
-	i, j = 0, 0
-	while (len(result) < len(left) + len(right)):
-		if left[i][v] < right[j][v]:
-			result.append(left[i])
-			i+= 1
-		else:
-			result.append(right[j])
-			j+= 1
-		if i == len(left) or j == len(right):
-			result.extend(left[i:] or right[j:])
-			break
-	return result
 
-def mergeSort(points, v):
-    if len(points) < 2:
-        return points
-
-    m = int(len(points)/2)
-    left = mergeSort(points[:m], 0)
-    right = mergeSort(points[m:], 0)
-    return merge(left, right, v)
 
 def dist(p1, p2):
     return sqrt(pow(p1[0]-p2[0],2) + pow(p1[1]-p2[1],2))
 
 def nearest_neighbor_recursion(points):
     min_distance=0
-    num = len(points)
+    length = len(points)
 
-    if num <= 3:
+    if length <= 3:
         return brute_force_nearest_neighbor(points)
     else:
-        m = int(num/2)          #middle index
-        left = points[:m]       #left half of points
-        right = points[m:]      #right half of points
+        c = int(length/2)
+        left = points[:c]
+        right = points[c:]
 
         med = right[0]
 
@@ -50,14 +28,11 @@ def nearest_neighbor_recursion(points):
             min_distance = ld
         else:
             min_distance = rd
-
         mid = []
-        ysort = mergeSort(points, 1)
-
-        for p in ysort:
-                if abs(p[0] - med[0]) < min_distance:
-                        mid.append(p)
-
+        sortsort = sorted(points, key=lambda x:x[1])
+        for k in sortsort:
+                if abs(k[0] - med[0]) < min_distance:
+                        mid.append(k)
         size = len(mid)
         if size > 1:
                 for i in range(size - 1):
@@ -66,29 +41,23 @@ def nearest_neighbor_recursion(points):
                                         min_distance = dist(mid[i], mid[j])
     return min_distance
 
-#Run the divide-and-conquor nearest neighbor
+# Run the divide-and-conquor nearest neighbor
 def nearest_neighbor(points):
-    points = mergeSort(points, 0)
+    points = sorted(points, key=lambda x:x[0])
     return nearest_neighbor_recursion(points)
 
 #Brute force version of the nearest neighbor algorithm, O(n**2)
 def brute_force_nearest_neighbor(points):
-    start_time = time.clock()
     min_distance=-1
     for i in range (0, len(points) - 1):
         for j in range (i+1, len(points)):
             d = dist(points[i], points[j])
             if (min_distance == -1) or (min_distance > d):
                 min_distance = d
-    print("--- %s seconds ---" % (time.clock() - start_time))
     return min_distance
 
 def read_file(filename):
     points=[]
-    # File format
-    # x1 y1
-    # x2 y2
-    # ...
     in_file=open(filename,'r')
     for line in in_file.readlines():
         line = line.strip()
@@ -99,23 +68,17 @@ def read_file(filename):
             points.append((x,y))
     return points
 
-def main(filename,algorithm):
-    algorithm=algorithm[0:]
+def main(filename):
     points=read_file(filename)
-    if algorithm =='dc':
-        print("Divide and Conquer: ", nearest_neighbor(points))
-    if algorithm == 'bf':
-        print("Brute Force: ", brute_force_nearest_neighbor(points))
-    if algorithm == 'both':
-        print("Divide and Conquer: ", nearest_neighbor(points))
-        print("Brute Force: ", brute_force_nearest_neighbor(points))
+    average = 0
+    start_time = time.clock()
+    distance = nearest_neighbor(points)
+    # distance = brute_force_nearest_neighbor(points)
+    tm = (time.clock() - start_time)
+    print("--- %s seconds ---" % tm)
+    text_write = open(filename + "_distance.txt", "w")
+    hello = str(distance)
+    text_write.write(hello)
+    text_write.close()
 
-
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print("python nearest_neighbor.py -<dc|bf|both> <input_file>")
-        quit(1)
-    if len(sys.argv[1]) < 2:
-        print("python nearest_neighbor.py -<dc|bf|both> <input_file>")
-        quit(1)
-    main(sys.argv[2],sys.argv[1])
+main(sys.argv[1])
